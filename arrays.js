d0772ff694062df97e23e0ebfb4c826bd1a471ec
Lucas <lucasnyboder64@gmail.com>
2026-09-01 10:17:50 +0200

    Opgave 4

--- arrays.js
+++ arrays.js
@@ -1,13 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let catsArray = ["Ollie", "Sophie", "Salem", "Tiger", "Binx", "Pumpkin", "Penny", "Lenny"];
     let dogsArray = ["Barney", "Molly", "Baxter", "Polly", "Buddy", "Suki", "Watson"];
     let fruitsArray = ["Banana", "Orange", "Pineapple", "Mango", "Plum", "Apricot"]
     let numbersArray = [12, 4, 45, 17, 2, 26, 7, 55, 48, 32, 21]
 
     // --- Løs opgaverne herunder --- 
-    
+
     // Opgave 1
-    let newNumbers = numbersArray.filter((num) => num>20);
+    let newNumbers = numbersArray.filter((num) => num > 20);
     console.log(newNumbers);
 
     // Opgave 2
@@ -15,6 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Opgave 2 " + catsArray);
 
     // Opgave 3
-    console.log("Opgave 3a "+dogsArray.includes("Trixie"));
-    console.log("Opgave 3b "+dogsArray.includes("Baxter"));
+    console.log("Opgave 3a " + dogsArray.includes("Trixie"));
+    console.log("Opgave 3b " + dogsArray.includes("Baxter"));
+
+    // Opgave 4
+    if (catsArray.includes("Bagheera")) {
+        console.log("Opgave 4a" + " Bagheera");
+    } else {
+        console.log(undefined);
+    }
+
+    if (catsArray.includes("Salem")) {
+        console.log("Opgave 4b" + " Salem");
+    } else {
+        console.log(undefined);
+    }
+
+
 }) // ends DOMContentLoaded
